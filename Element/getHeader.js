2336107ef40acd2a7e4f59e6db2f3a0b32acdfe0
YuukiConan <97886300+YuukiConan@users.noreply.github.com>
2024-10-27 20:14:43 +0700

    Update getHeader.js

--- Element/getHeader.js
+++ Element/getHeader.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    fetch("Element/header.html") 
+    fetch("header.html") 
     .then(response => response.text())
     .then(data => {
             document.querySelector('header').innerHTML = data;
